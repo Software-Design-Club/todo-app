b7fefd5f395a24e007e96406d95ab80db7917787
Emmanuel Genard <edgenard@gmail.com>
2024-09-29 12:48:16 -0400

    Add Drizzle ORM and make users table

---
+++ drizzle/schema.ts
@@ -0,0 +1,29 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+
+export const userStatusEnum = pgEnum("status", ["active", "deleted"]);
+
+export const UsersTable = pgTable(
+  "todo_users",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull(),
+    status: userStatusEnum("status").default("active").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(users.email),
+    };
+  }
+);
