f53b069a1cbc991185c972a499e17428253137cc
Emmanuel Genard <edgenard@gmail.com>
2024-09-29 13:44:21 -0400

    Add basic authentication with Github

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <SignIn />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
