66d93450ebf2d22cbf2cf0f0a98ba740b6df2bae
Emmanuel Genard <edgenard@gmail.com>
2024-10-20 17:05:51 -0400

    Comment out unused function

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import { auth } from "@/auth";
 
 import Header from "@/components/header";
 import Link from "next/link";
-import { getListsWithTodos } from "./actions";
+// import { getListsWithTodos } from "./actions";
 
 export default async function Home() {
   const session = await auth();
   const user = session?.user;
-  let listsWithTodos = [];
-  if (user?.email) {
-    listsWithTodos = await getListsWithTodos({ email: user?.email });
-  }
+  // let listsWithTodos = [];
+  // // if (user?.email) {
+  // //   listsWithTodos = await getListsWithTodos({ email: user?.email });
+  // // }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
